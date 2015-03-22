58fcbfafb5d62161e93fe04dacf279899b086298
Brian Kenny <bkenny@gmailcom>
2015-03-21 22:27:02 -0400

    Work in progress on loadChannelVideos function

--- app/controllers/channels.server.controller.js
+++ app/controllers/channels.server.controller.js
@@ -9,9 +9,41 @@ var _ = require('lodash'),
     Channel = mongoose.model('Channel');
 
 
-exports.loadChannelData = function(req, res) {
+var requestHttps = function(channelId,publishedAfter,nextPageToken, cb) {
+    
+    var https = require('https');
+    var options = {
+        host: 'www.googleapis.com',
+        path: '/youtube/v3/search?key=AIzaSyAhvqkZmykgopZc990N7NQvWGUkNEDlHes'
+    };
 
-    var returned = [];
+    options.path += ('&part=snippet');
+    options.path += ('&maxResults=50');
+    options.path += ('&channelId=' + channelId);
+    options.path += ('&publishedAfter=' + publishedAfter);
+    if (nextPageToken) {
+        options.path += ('&nextPageToken=' + nextPageToken);
+    }
+
+    var callback = function(response) {
+        var request_data = '';
+
+        response.on('data', function(chunk) {
+            request_data += chunk;
+        });
+
+        response.on('end', function() {
+            if (cb && typeof cb === 'function') {
+                cb(request_data);
+            }
+        });
+    };
+
+    https.request(options,callback).end();
+
+};
+
+exports.loadChannelData = function(req, res) {
 
     var channelDataSet = [
         {
@@ -24,14 +56,40 @@ exports.loadChannelData = function(req, res) {
         var channelData = channelDataSet[i];
         Channel.findOne({ 'name':channelData.name }, 'name', function(err, channel) {
             if (err) console.log(err);
-            if (channel) console.log(channelData.name + ' already exists');
+            if (channel) console.log(channelData.name + ' channel already exists');
             else {
                 new Channel(channelData).save();
-                console.log(channelData.name + 'added');
+                console.log(channelData.name + ' channel added');
             }
         });
     }
     res.send('OK');
 
 };
+
+exports.loadChannelVideos = function(req, res) {
+    var channelName = req.query.channel;
+
+    requestHttps('UC1UzB_b7NSxoRjhZZDicuqw', '2015-01-01T00:00:00Z', undefined, function(data) {
+        var json_data = JSON.parse(data);
+        if (json_data.nextPageToken) {
+            console.log(JSON.stringify(json_data.nextPageToken));
+            requestHttps('UC1UzB_b7NSxoRjhZZDicuqw','2015-01-01T00:00:00Z', json_data.nextPageToken, this); 
+        }
+        else {
+            res.send('OK');
+        }        
+        res.send(JSON.stringify(json_data.nextPageToken));    
+    });
+    
+
+    Channel.findOne({'name':channelName}, 'name youtube_id last_updated', function(err, channel) {
+        if (err) console.log(err);
+        if (channel) {
+
+        }
+    });
+
+};
+
 
--- app/routes/videos.server.routes.js
+++ app/routes/videos.server.routes.js
@@ -3,5 +3,8 @@
 module.exports = function(app) {
     // Root routing
     var videos = require('../../app/controllers/videos.server.controller');
+    var channels = require('../../app/controllers/channels.server.controller');
     app.route('/videos').get(videos.getVideos);
+    app.route('/load_channel_data').get(channels.loadChannelData);
+    app.route('/load_channel_videos').get(channels.loadChannelVideos);
 };
