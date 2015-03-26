c9304b29c1466f073e062774553d620bb2285b7a
elennick <evan.lennick@accolade.com>
2015-03-25 21:57:02 -0400

    parsing player and character info from the server if its available

--- public/modules/core/directives/videorow.client.directive.js
+++ public/modules/core/directives/videorow.client.directive.js
@@ -6,7 +6,30 @@ angular.module('core').directive('videorow', [
             scope: {
                 video: '='
             },
-            templateUrl: 'modules/core/directives/videorow.client.template.html'
+            templateUrl: 'modules/core/directives/videorow.client.template.html',
+            link: function(scope, element, attrs, controller) {
+                scope.video.game = 'Ultra Street Fighter 4';
+                scope.video.type = 'Unknown';
+                scope.video.event = 'Unknown';
+                scope.video.playersString = 'Unknown';
+                scope.video.charactersString = 'Unknown';
+
+                for(var i = 0; i < scope.video.players.length; i++) {
+                    if(i == 0) {
+                        scope.video.playersString = scope.video.players[i].player;
+                    } else {
+                        scope.video.playersString += ', ';
+                        scope.video.playersString += scope.video.players[i].player;
+                    }
+
+                    if(i == 0) {
+                        scope.video.charactersString = scope.video.players[i].character;
+                    } else {
+                        scope.video.charactersString += ', ';
+                        scope.video.charactersString += scope.video.players[i].character;
+                    }
+                }
+            }
         };
     }
 ]);
